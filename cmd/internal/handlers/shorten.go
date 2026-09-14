5501b008fb403fa5985940994271ce511a8bcfc3
Rajit Dutta <rajitdutta2024@gmail.com>
2026-09-14 20:11:24 +0530

    implemented rate limiting logic

--- cmd/internal/handlers/shorten.go
+++ cmd/internal/handlers/shorten.go
@@ -2,11 +2,16 @@ package handlers
 
 import (
 	"encoding/json"
+	"net"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/Rajit-Dutta/go-redis-url-shortener/cmd/api/helpers"
+	"github.com/Rajit-Dutta/go-redis-url-shortener/cmd/internal/config"
+	"github.com/Rajit-Dutta/go-redis-url-shortener/cmd/internal/db"
 	"github.com/Rajit-Dutta/go-redis-url-shortener/cmd/internal/httpx"
+	"github.com/redis/go-redis/v9"
 )
 
 type request struct {
@@ -27,6 +32,32 @@ func ShortenURL(w http.ResponseWriter, r *http.Request) {
 	var req request
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		httpx.Error(w, http.StatusBadRequest, "Something went wrong while reading body", "invalid_body")
+	}
+
+	//implementation of rate limiting
+	r2 := db.CreateClient(1)
+	defer r2.Close()
+
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		host = r.RemoteAddr
+	}
+
+	val, err := r2.Get(db.Ctx, host).Result()
+	if err == redis.Nil {
+		err = r2.Set(db.Ctx, host, config.MustLoad().Quota, time.Second*60*30).Err()
+		if err != nil {
+			httpx.Error(w, http.StatusInternalServerError, "Something went wrong during IP fetching", "unsuccesful_fetch")
+		}
+	} else {
+		valInt, err := strconv.Atoi(val)
+		if err != nil {
+			httpx.Error(w, http.StatusInternalServerError, "Something went wrong during host conversion to INT", "unsuccesful_conversion")
+		}
+		if valInt == 0 {
+			ttl, _ := r2.TTL(db.Ctx, host).Result()
+			httpx.RateLimitRestError(w, http.StatusBadRequest, "Rate limit exceeded", "rate_limit_exceeded", time.Duration(ttl.Seconds()))
+		}
 	}
 
 	//check if actual URL
--- cmd/internal/httpx/error.go
+++ cmd/internal/httpx/error.go
@@ -3,12 +3,14 @@ package httpx
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 )
 
 type ErrorPayload struct {
-	Field   string `json:"field,omitempty"`
-	Code    string `json:"code"`
-	Message string `json:"msg"`
+	Field         string        `json:"field,omitempty"`
+	Code          string        `json:"code"`
+	Message       string        `json:"msg"`
+	RateLimitRest time.Duration `json:"rate_limit_rest,omitempty"`
 }
 
 type ErrorWrapper struct {
@@ -23,6 +25,19 @@ func Error(w http.ResponseWriter, status int, message string, code string) {
 		ErrorPayload{
 			Code:    code,
 			Message: message,
+		},
+	})
+}
+
+func RateLimitRestError(w http.ResponseWriter, status int, message string, code string, rateLimit time.Duration) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	json.NewEncoder(w).Encode(ErrorWrapper{
+		ErrorPayload{
+			Code:          code,
+			Message:       message,
+			RateLimitRest: rateLimit,
 		},
 	})
 }
